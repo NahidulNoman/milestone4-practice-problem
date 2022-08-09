725273b394ace1eba6532a3a58988a88d1c9e65e
NahidulNoman <nahidulna5@gmail.com>
2022-08-09 12:11:36 +0600

    first-30-solved

---
+++ first-30-problem.js
@@ -0,0 +1,271 @@
+// milestone all problem try to solve this here we go ---
+
+// 1) Harry’s mom gave him tk 1000 and asked him to buy some oranges and apples. Write a program to help Harry calculate how much money the shopkeeper will return. The total cost of 1 kg of oranges and 1 kg of apples is tk 700. 
+
+// let momGiven = 1000;
+// let apples = 400;
+// let oranges = 300;
+// let totalBuy = apples + oranges;
+// let shopkeeperReturn = momGiven - totalBuy ;
+// console.log(shopkeeperReturn);
+
+// 2) Write a program to calculate the average marks of Mathematics, Biology, Chemistry, Physics, and Bangla of a student.
+
+// let math = 89;
+// let biology = 86;
+// let physics = 71;
+// let bangla = 80;
+// let totalMarks = math + biology + physics + bangla
+// let average = totalMarks / 4;
+// console.log(average);
+
+// 3) John’s teacher gave him two variables. Each variable contains a string.John’s teacher asked him to combine these two strings(‘I am going to be’ and ‘an awesome web developer’) and print them in one line. Help John write the program.
+    
+// let firstLine = 'I am going to be ';
+// let secondLine = 'an awesome web developer'
+// let fullLine = firstLine + secondLine
+// console.log(fullLine);
+
+// 4) Sarah’s mother is teaching her mathematics. She gave Sarah the number 119 and asked her what the remainder would be if she divided the number by 5. Help Sarah write the program.
+
+// let giveNumber = 119;
+// let divided = 5
+// let remainder = giveNumber % divided
+// console.log(remainder);
+
+// 5) নি চে র ভে রি য়ে বল ডি ক্লে য়ার এ ক োনটার মধ্যে কি কি সমস্যা আছে । দে খত ো বে র করতে
+// পার ো কি না?
+
+// Var price = 33
+// var name - Shabana
+// var boxName = ‘Cocola;
+// var 88_price = 34;
+// var enum = -1;
+// var _$box’78 = ‘Monika’;
+// var home-address = “kochu khet”;
+
+// 6) You are given an array:
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// a) Find the index of ‘Banana’ and replace ‘Banana’ with ‘Mango’. b) Remove ‘Orange’ and add ‘Watermelon’.
+
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// let food = fruits[1];
+// fruits[1]= 'Mango';
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.push('Watermelon');
+// console.log(fruits);
+
+// 7) You and your friends Tom, Jane, Peter and John got their final exam results. Your total score is 85, Tom’s total score is 66, Jane’s total score is 95, Peter’s total score is 56 and John’s total score is 40. The grading chart is
+
+// let marks = 35;
+// if (marks > 80 && 80 <100){
+//     console.log('you got A+')
+// }
+// else if(marks > 70 && 70 < 100){
+//     console.log('you got A')
+// }
+// else if (marks > 60 && 60 < 100){
+//     console.log('you got A-')
+// }
+// else if (marks > 50 && 50 < 100){
+//     console.log('you got B')
+// }
+// else if (marks > 40 && 40 < 100){
+//     console.log('you got C')
+// }
+// else{
+//     console.log('you fail try next time!!')
+// }
+
+// 8) You are given three numbers 13, 79, and 45. Write a program that will print the largest number using if-else.
+
+// let num1 = 13;
+// let num2 = 79;
+// let num3 = 45;
+// if ( num1> num2 && num1> num3){
+//     console.log('Yes number One is largest!!!')
+// }
+// else if (num2 > num1 && num2 > num3){
+//     console.log('Yes number Two is largest!!!')
+// }
+// else{
+//     console.log('Yes number three is largest!!!')
+// }
+
+// 9) You are given a triangle with the sides 9, 8, 9. Write a program to check whether a triangle is Isosceles or not using if-else. Isosceles => two sides are equal
+
+// same as number 8
+
+// 10) ক্লাস সে ভে র এর ফাইনাল এক্সাম এ তুমি ফার্স্ট হয়ে ছ। ত োমার বন্ধুআলি য়া, ডালি য়া,সালি য়া, মালি য়া, লি লি য়া আর জ্বালাইয়া - তাদে র grade তুমি জান ো না। তবে তাদে র নম্বর জান ো. আলি য়া 95 পে য়ে ছে , ডালি য়া 66 পে য়ে ছে , সালি য়া 80 পে য়ে ছে , মালি য়া পে য়ে ছে 59,লি লি য়া পে য়ে ছে 47, জ্বালাইয়া পে য়ে ছে 77। তুমি JS code দি য়ে তাদে র grade বে র করেদি তে পারবে ?
+
+// same as number 7
+
+// 11) তাড়াহুড়া করে স্কুলের জন্য বের হচ্ছ কিন্তু রাস্তা পার হওয়ার সময় দেখলে , ট্রাফিক সিগন্যাল লাল রং। এই অবস্থায় যদি তুমি রাস্তা পার হওয়ার চেষ্টা কর, তাহলে ডেঞ্জার হতে পারে । যদি হলদু রং হয় তাহলে tumar থেমে যাওয়া উচিত। আর যদি ট্রাফিক সিগন্যাল গ্রিন হয় তাহলে ত োমার রাস্তা পার হওয়া উচিত। তাই একটা ক োড লিখে ফেল ো। যেখানে আমরা signal নামে একটা ভেরিয়েবল থাকবে । সেটার মান green, yellow বা red হতে পারে ।সেই অনসুারে ডিফারেন্টডি ফারেন্ট কাজ হবে । ত ো, সে ই ক োড ফটাফট লিখে ফেল ো।
+
+// let signal1 = 'green'
+// let signal2 = 'yellow'
+// let signal3 = 'red'
+
+// 12) 10 times for looping this array--
+
+// let myStudy = ['unlock', 'video', 'practice', 'support'];
+// for(i = 0; i < 10; i++){
+    
+//    console.log(myStudy)
+// }
+
+// 13) 10 times while looping this array--
+
+// let myStudy = ['unlock', 'video', 'practice', 'support'];
+// let i = 0;
+// while(i < 10){
+//     console.log(myStudy);
+//     i++;
+// }
+
+// 14) this array 10 times reverse way using for loop
+
+// let myStudy = ['unlock1', 'video2', 'practice3', 'support4'];
+// for(i = myStudy.length; i > 0; i--){
+//     console.log(myStudy)
+// }
+
+// 15) this array 10 times reverse way using while loop
+
+// 16) ত োমার কাছে : ৮০০০০ টাকার বেশি হলে তুমি mac কি নবে , ৬০ টাকার বে শি হলে gaming ল্যাপটপ কি নবে , ৪০ হাজার টাকার বে শি হলে lenovo yoga কম্পিউটার কি নবে , ২০ হাজার টাকার বে শি হলে পুরান ল্যাপটপ কি নবে । না হয় তুমি ম োবাইল দি য়ে কাজ চালাবে ।
+
+// var myMoney = 30000;
+
+// if(myMoney > 80000){
+//     console.log('tumi mac kinte parba')
+// }
+// else  if(myMoney > 60000 ){
+//     console.log('tumi ekta gaming laptop kinte paro')
+// }
+// else if(myMoney > 40000 ){
+//     console.log('tumi lenovo giga computer kinte paro')
+// }
+// else if( myMoney > 20000){
+//     console.log('tumi ekta puran laptop kinte paro')
+// }
+// else{
+//     console.log('mobile diya kaj chalao')
+// }
+
+// 17) আসকে আমার মন ভাল ো নে ই এই কথা ৩৯ বার আউটপুট 
+// হিসেবে দেখাও।
+
+// for(i = 0; i <= 39; i++ ){
+//     console.log('Ajke amar mon valo nai')
+// }
+
+// same problem just use while loop--
+
+// let mon = 0;
+// while(mon <=39){
+//     console.log('Ajke amar mon valo nai use while loop!!');
+//     mon++;
+// }
+
+// 19) একটা ক োড লি খে ৫৮ থে কে ৯৮ পর্যন্ত যত সংখ্যা আছে সে গুলাকে দে খাও
+
+// for (i = 58; i <= 98; i++){
+//     console.log(i);
+// }
+
+// 20) একটা ক োড লিখে ৪১২ থেকে ৪৫৬ পর্যন্ত যত জ োর সংখ্যা আছে সে গুলাকে দেখাও
+
+// for(i = 412; i <= 456; i=i+2){
+//     console.log(i);
+// }
+
+// 21) একটা ক োড লি খে ৫৮১ থে কে ৬২৩ পর্যন্ত যত বি জ োড় সংখ্যা আছে সে গুলাকে দে খাও
+
+// for (i = 581; i<= 623; i=i+2){
+//     console.log(i);
+// }
+
+// 22) তুমি এতদিন যা যা জিনিস শিখছ ো সে গুলার নাম দিয়ে একটা array বানাও। তারপর একটা for লপু দিয়ে সেই array এর সব উপাদান কে আউটপুট হিসেবে দেখাও।
+
+// let thoseWeLearn = ['variabel','conditionals','loop','array','function','object'];
+// for(i = 0; i < thoseWeLearn.length; i++){
+//     let index = thoseWeLearn[i];
+//     console.log(index);
+// }
+
+// 23) তুমি এতদিন পর্যন্ত যে যে মডেলের ম োবাইল ফ োন ইউজ করেছ ো সে গুলার নাম দিয়ে একটাarray বানাও। তারপর একটা while লপু দিয়ে সেই array এর উপাদান গুলা একটা একটা করে আউটপুট হিসেবে দেখাও
+
+// let thoseIUsePhone = ['xiaomi','alketel-onetouch','nokia'];
+// let i = 0;
+// while(i < thoseIUsePhone.length ){
+//     let usePhone = thoseIUsePhone[i];
+//     console.log(usePhone);
+//     i++;
+// }
+
+// 24) একটা ফর লপু চালাও। ৩০ থে কে ৮৬ পর্যন্ত। আর এই লপু ৪৪ এ গেলে ব্রেক করবে । সে ই জিনিস ক োড করে দে খাও
+
+// for(i = 30; i <= 86; i++){
+//     console.log(i);
+//     if( i >= 44){
+//         break;
+//     }
+// }
+
+// 25) ত োমার যত বই আছে সে গুলার দাম নিয়ে একটা array লিখে ফে ল ো। যে বই গুল োর দাম ২০০ টাকার উপরে সে গুলাকে স্কিপ করবে । অর্থাৎ সে গুলাকে আউটপুট হিসেবে দেখাবে না। বাকি দের কেআউটপুট হিসেবে দেখাবে । দেখ ো করতে পার ো কি না।
+
+// let bookPrice = [155, 343, 567, 180, 120, 90, 198, 220, 560];
+// for(i = 0; i < bookPrice.length; i++){
+//     let index = bookPrice[i];
+//     if(index > 200){
+//         continue;
+//     }
+//     console.log(index);
+// }
+
+// 26) Write a function called foo() which prints “foo” and a function called bar() which prints “bar”. Call function bar() in the foo() function after printing. Whatwill be the output? Now call the foo() to see the output.
+
+// function foo(foO){
+//     console.log('foo')
+//     function bar(baR)
+// }
+// foo(foO)
+
+// 27) Write a function called make_avg() which will take an three integers and return the average of those values.
+
+// function make_avg(num1,num2,num3){
+//     let sum = num1 + num2 + num3;
+//     let average = sum / 3;
+//     return average;
+// }
+// let getResult = make_avg(10,20,30);
+// console.log(getResult);
+
+// 28) Challenging: Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values
+
+// function make_avg(){
+
+// }
+// let averArray = [10,20,30,40,50];
+// let getResult = make_avg(averArray);
+// console.log(getResult);
+
+// 29) Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. You need to do it in 4 ways:
+// ● Has return + Has parameter
+// ● No return + Has parameter
+
+// function odd_even(number){
+//     let valueNumber = number % 2;
+//     if(valueNumber == 0){
+//         return true
+//     } 
+// }
+// let oddEvenNumber = 50;
+// let getResult = console.log(oddEvenNumber);
+// console.log(getResult);
+
+
+// 30) You are in a hurry to go to your school on time. But when you are crossingthe road, the traffic signal is red coloured. In this situation, if you try to cross theroad, you may be in danger.If you notice a yellow coloured traffic signal, you should stop. If you notice a green coloured traffic signal, you should cross the road. So write a JS code, where there is a variable called signal. Its value can be green, yellow or red & we will get different activities as output depending on the variable. So, hurry up.
+
